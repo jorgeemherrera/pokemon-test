f0efcef053e0c4b648e88a42fba842088ab8b55c
Jorge Eliécer Muñoz Herrera <jorge.m.h@outlook.com>
2025-11-08 15:18:49 -0500

    refactor: add SCSS architecture (mixins, variables, and utils)

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,10 @@
+import './Home.scss';
+
+export const Home = () => {
+  return (
+    <div className='home'>
+      Home Page
+    </div>
+  )
+}
+export default Home; 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true,
+        additionalData: `@use '@styles/utils' as *;`,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+    },
+  },
+});
